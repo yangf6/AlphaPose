8829a43affc6bee51c4a309a0e86a82e36cdc7e1
Linda Yang <yangf6@uw.edu>
2021-10-26 23:06:01 -0700

    update detector setting

--- detector/apis.py
+++ detector/apis.py
@@ -8,24 +8,9 @@ from abc import ABC, abstractmethod
 
 
 def get_detector(opt='None'):
-    if opt is None:
-        from detector.yolo_api import YOLODetector
-        from detector.yolo_cfg import cfg
-        return YOLODetector(cfg, opt)
-    elif opt.detector == 'yolo':
-        from detector.yolo_api import YOLODetector
-        from detector.yolo_cfg import cfg
-        return YOLODetector(cfg, opt)
-    elif opt.detector == 'tracker':
-        from detector.tracker_api import Tracker
-        from detector.tracker_cfg import cfg
-        return Tracker(cfg, opt)
-    elif opt.detector.startswith('efficientdet_d'):
-        from detector.effdet_api import EffDetDetector
-        from detector.effdet_cfg import cfg
-        return EffDetDetector(cfg, opt)
-    else:
-        raise NotImplementedError
+    from detector.yolo_api import YOLODetector
+    from detector.yolo_cfg import cfg
+    return YOLODetector(cfg, opt)
 
 
 class BaseDetector(ABC):
