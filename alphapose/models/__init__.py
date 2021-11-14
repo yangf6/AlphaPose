20efa3b9104a45770b7720fe337cbab25a2284d0
Linda Yang <yangf6@uw.edu>
2021-11-14 12:02:51 -0800

    update cpu version

--- alphapose/models/__init__.py
+++ alphapose/models/__init__.py
@@ -1,9 +1,9 @@
 from .fastpose import FastPose
 # from .fastpose_duc import FastPose_DUC
-from .hrnet import PoseHighResolutionNet
-from .simplepose import SimplePose
+# from .hrnet import PoseHighResolutionNet
+# from .simplepose import SimplePose
 # from .fastpose_duc_dense import FastPose_DUC_Dense
-from .hardnet import HarDNetPose
-from .criterion import L1JointRegression
+# from .hardnet import HarDNetPose
+# from .criterion import L1JointRegression
 
-__all__ = ['FastPose', 'SimplePose', 'PoseHighResolutionNet','HarDNetPose', 'L1JointRegression']
+__all__ = ['SimplePose']
